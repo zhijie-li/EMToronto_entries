dc06e54666cf45f1129dede00a172a0412de88d5
Zhijie Li <27743673+zhijie-li@users.noreply.github.com>
2022-03-05 18:26:27 -0500

    Create del_cs2_jobs.sh

---
+++ del_cs2_jobs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os,sys
+prj='P9'
+action='delete_job'
+
+j=[]
+if len(sys.argv)>1:
+    for i in sys.argv[1:]:
+
+        if len(i)<=4 :
+            j.append(i)
+        elif '-' in i :
+            a,b=i.split('-')
+            j.extend(range(int(a),int(b)+1))
+
+for i in j:
+
+    os.system("cryosparcm cli '{}(\"{}\",\"J{}\" )'".format(prj,action,i))
+    
